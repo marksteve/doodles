da295a5cc2a2aac5c37bb10ebde264805a5f7132
Mark Steve Samson <hello@marksteve.com>
2022-06-22 10:14:59 +0800

    Initial commit

---
+++ app/routes/index.tsx
@@ -0,0 +1,23 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Doodles</h1>
+      <ul>
+        <li><a href="https://codepen.io/marksteve/pen/nqPYjm">Forebruary</a></li>
+        <li><a href="https://marksteve.com/sketches/001_thinkingmachines/">Animated Logo</a></li>
+        <li><a href="https://github.com/marksteve/citd">Code in the Dark</a></li>
+        <li><a href="https://github.com/marksteve/robattler">Robattler</a></li>
+        <li><a href="https://github.com/marksteve/SudoXE">SudoXE</a></li>
+        <li><a href="https://speedhack.web.app/">Speedhack</a></li>
+        <li><a href="https://github.com/marksteve/yestermap">Yestermap</a></li>
+        <li><a href="https://github.com/marksteve/vday">Valentine's Day</a></li>
+        <li><a href="https://github.com/marksteve/map-challenge">Maps</a></li>
+        <li><a href="https://github.com/marksteve/toggdle">Toggdle</a></li>
+        <li><a href="https://github.com/marksteve/cards">Dos</a></li>
+        <li><a href="https://github.com/marksteve/nodevember">Nodevember</a></li>
+        <li><a href="https://github.com/marksteve/araw-araw">Araw-araw</a></li>
+        <li><a href="https://github.com/marksteve/jomel.draws">jomel.draws</a></li>
+      </ul>
+    </div>
+  );
+}
